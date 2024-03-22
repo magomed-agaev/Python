7dd984254cb075617992d0050fc21255cf7dfec1
kenny-ali-source <kenny.ali@laplateforme.io>
2024-03-22 10:42:38 +0100

    changement d'import par tkinter dans la classe Game

--- Game.py
+++ Game.py
@@ -1,20 +1,18 @@
-import pygame
-from Case import Case
-from Board import Board 
-import os
+import tkinter as tk
+from Board import Board
 from Solver import Solver
-from time import sleep
+import os
 
 class Game:
     def __init__(self, size, prob):
+        self.root = tk.Tk()
         self.board = Board(size, prob)
-        pygame.init()
         self.sizeScreen = 800, 800
-        self.screen = pygame.display.set_mode(self.sizeScreen)
+        self.canvas = tk.Canvas(self.root, width=self.sizeScreen[0], height=self.sizeScreen[1])
+        self.canvas.pack()
         self.pieceSize = (self.sizeScreen[0] / size[1], self.sizeScreen[1] / size[0]) 
         self.loadPictures()
         self.solver = Solver(self.board)
-        self.Case = Case(self.hasBomb)
 
     def loadPictures(self):
         self.images = {}
@@ -22,46 +20,42 @@ class Game:
         for fileName in os.listdir(imagesDirectory):
             if not fileName.endswith(".png"):
                 continue
-            path = imagesDirectory + r"/" + fileName 
-            img = pygame.image.load(path)
-            img = img.convert()
-            img = pygame.transform.scale(img, (int(self.pieceSize[0]), int(self.pieceSize[1])))
+            path = os.path.join(imagesDirectory, fileName)
+            img = tk.PhotoImage(file=path)
             self.images[fileName.split(".")[0]] = img
             
     def run(self):
         running = True
         while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            self.root.update()
+            for event in self.root.event:
+                if event.type == tk.QUIT:
                     running = False
-                if event.type == pygame.MOUSEBUTTONDOWN and not (self.board.getWon() or self.board.getLost()):
-                    rightClick = pygame.mouse.get_pressed(num_buttons=3)[2]
-                    self.handleClick(pygame.mouse.get_pos(), rightClick)
-                if event.type == pygame.KEYDOWN:
+                if event.type == tk.MOUSEBUTTONDOWN and not (self.board.getWon() or self.board.getLost()):
+                    rightClick = event.num == 3
+                    self.handleClick((event.x, event.y), rightClick)
+                if event.type == tk.KEYDOWN:
                     self.solver.move()
-            self.screen.fill((0, 0, 0))
+            self.canvas.delete("all")
             self.draw()
-            pygame.display.flip()
             if self.board.getWon():
                 self.win()
                 running = False
-        pygame.quit()
         
     def draw(self):
         topLeft = (0, 0)
         for row in self.board.getBoard():
             for piece in row:
-                rect = pygame.Rect(topLeft, self.pieceSize)
                 image = self.images[self.getImageString(piece)]
-                self.screen.blit(image, topLeft) 
+                self.canvas.create_image(topLeft[0], topLeft[1], anchor="nw", image=image) 
                 topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
             topLeft = (0, topLeft[1] + self.pieceSize[1])
 
     def getImageString(self, piece):
         if piece.getClicked():
             return str(piece.getNumAround()) if not piece.getHasBomb() else 'bomb-at-clicked-block'
-        if (self.board.getLost()):
-            if (piece.getHasBomb()):
+        if self.board.getLost():
+            if piece.getHasBomb():
                 return 'unclicked-bomb'
             return 'wrong-flag' if piece.getFlagged() else 'empty-block'
         return 'flag' if piece.getFlagged() else 'empty-block'
@@ -71,6 +65,10 @@ class Game:
         self.board.handleClick(self.board.getPiece(index), flag)
 
     def win(self):
-        sound = pygame.mixer.Sound('win.wav')
-        sound.play()
-        sleep(3)
+        win_label = tk.Label(self.root, text="You Win!", font=("Helvetica", 24))
+        win_label.pack()
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    game = Game((10, 10), 0.1)
+    game.run()
